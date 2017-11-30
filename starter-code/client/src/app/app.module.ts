e3c8543b1fec44a93fbf7baada325e6db83a3830
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-30 16:13:44 +0100

    fixed angular devkit schematics

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListEntryComponent } from './list-entry/list-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEntryComponent
   ],
   imports: [
     BrowserModule,
